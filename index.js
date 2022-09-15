5a32085f8c060946b5154319637b66cf4a588c65
ManuelDev <manueldeveloper17@gmail.com>
2022-09-14 21:18:03 -0500

    Methos map

--- index.js
+++ index.js
@@ -29,3 +29,26 @@ const characters = [
     gender: "male",
   },
 ];
+
+/* MAP */
+
+// 1. Get an array of all names
+const allNames = characters.map((character) => character.name);
+// console.log(allNames);
+
+// 2. Get an array of all heights
+const allHeights = characters.map((character) => character.height);
+// console.log(allHeights);
+
+// 3. Get an array of objects with just name and height properties
+const nameAndHeight = characters.map((character) => ({
+  name: character.name,
+  height: character.height,
+}));
+// console.log(nameAndHeight);
+
+// 4. Get an array of all first names
+const allFirstNames = characters.map(
+  (characters) => characters.name.split(" ")[0]
+);
+console.log(allFirstNames);
